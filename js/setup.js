63ff02be41add81d11861175c709b101fe67ee75
AlexyLexto92 <lexto92@gmail.com>
2019-06-20 11:09:01 +0300

    исправление по замечаниям

--- js/setup.js
+++ js/setup.js
@@ -12,29 +12,7 @@ var soName = ['да Марья', 'Верон', 'Мирабелла', 'Вальц
 var cloathColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var colorOfEyes = ['black', 'red', 'blue', 'yellow', 'green'];
 var fireballColor = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-/*
-var wizardProperties = [{
-    name: names[Math.floor(Math.random() * names.length)] + ' ' + soName[Math.floor(Math.random() * soName.length)],
-    coatColor: cloathColor[Math.floor(Math.random() * cloathColor.length)],
-    eyesColor: colorOfEyes[Math.floor(Math.random() * colorOfEyes.length)]
-  },
-  {
-    name: names[Math.floor(Math.random() * names.length)] + ' ' + soName[Math.floor(Math.random() * soName.length)],
-    coatColor: cloathColor[Math.floor(Math.random() * cloathColor.length)],
-    eyesColor: colorOfEyes[Math.floor(Math.random() * colorOfEyes.length)]
-  },
-  {
-    name: names[Math.floor(Math.random() * names.length)] + ' ' + soName[Math.floor(Math.random() * soName.length)],
-    coatColor: cloathColor[Math.floor(Math.random() * cloathColor.length)],
-    eyesColor: colorOfEyes[Math.floor(Math.random() * colorOfEyes.length)]
-  },
-  {
-    name: names[Math.floor(Math.random() * names.length)] + ' ' + soName[Math.floor(Math.random() * soName.length)],
-    coatColor: cloathColor[Math.floor(Math.random() * cloathColor.length)],
-    eyesColor: colorOfEyes[Math.floor(Math.random() * colorOfEyes.length)]
-  },
-];
-*/
+
 var wizardProperties = [];
 
 function generateWizards(targetArray, count) {
@@ -48,15 +26,7 @@ function generateWizards(targetArray, count) {
   return targetArray;
 }
 generateWizards(wizardProperties, 4);
-/*
-for (var i = 0; i < wizardProperties.length; i++) {
-  var wizardElement = templateElement.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizardProperties[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizardProperties[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizardProperties[i].eyesColor;
-  SimilarList.appendChild(wizardElement);
-}
-*/
+
 var renderWizard = function (Properties) {
   var wizardElement = templateElement.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = Properties.name;
@@ -79,10 +49,16 @@ var ENTER_KEYCODE = 13;
 var buttonOpenSetup = document.querySelector('.setup-open');
 var setupPopup = document.querySelector('.setup');
 var buttonCloseSetup = setupPopup.querySelector('.setup-close');
+//  присваивание tabindex=0 для кнопки закрытия окна
+buttonCloseSetup.setAttribute('tabindex', 0);
 var buttonOpenSetupIcon = document.querySelector('.setup-open-icon');
 buttonOpenSetupIcon.setAttribute('tabindex', 0);
 var setupUserName = setupPopup.querySelector('.setup-user-name');
+//  задаём форме атрибут minlength
+setupUserName.setAttribute('minlength', 2);
 var setupForm = setupPopup.querySelector('.setup-wizard-form');
+//  задаём форме атрибут action
+setupForm.setAttribute('action', 'https://js.dump.academy/code-and-magick');
 //  весь волшебник
 var setupWizard = setupPopup.querySelector('.setup-player');
 //  цвет мантии
@@ -103,72 +79,54 @@ function randomColor(intArray) {
   return randColor;
 }
 
-
-//  записываем в функцию все действия в открытом окне
-var onButtonClick = function () {
-  //  присваивание tabindex=0 для кнопки закрытия окна
-  buttonCloseSetup.setAttribute('tabindex', 0);
-
-  //  задаём форме атрибут action
-  setupForm.setAttribute('action', 'https://js.dump.academy/code-and-magick');
-
-  //  задаём форме атрибут minlength
-  setupUserName.setAttribute('minlength', 2);
-
-  //  реализация закрытия окна через нажатие кнопоки ESC
-  document.addEventListener('keydown', function (evt) {
-    //  проверяем setupUserName- елемент ли сейчас в фокусе
-    if (document.activeElement === setupUserName) {
-      //  если да -- не делаем ничего
-      return evt;
-    }
+//  реализация закрытия окна через нажатие кнопоки ESC
+document.addEventListener('keydown', function (evt) {
+  //  проверяем setupUserName- елемент ли сейчас в фокусе
+  if (document.activeElement === setupUserName) {
+    //  если да -- не делаем ничего
+    evt.preventDefault();
+  } else if (evt.keyCode === ESC_KEYCODE) {
     //  если нет-закрываем окно
-    if (evt.keyCode === ESC_KEYCODE) {
-      setupPopup.classList.add('hidden');
-    }
-    return evt;
-
-  });
-
-  //  изменение цвета мантии по клику
-  wizardCoat.addEventListener('click', function () {
-    wizardCoat.style.fill = randomColor(cloathColor);
-    wizardCoatInput.value = randomColor(cloathColor);
-
-  });
-  //  изменение цвета глаз по клику
-  wizardEyes.addEventListener('click', function () {
-    wizardEyes.style.fill = randomColor(colorOfEyes);
-    wizardEyesInput.value = randomColor(colorOfEyes);
-  });
-  //  изменение цвета фаейрбола по клику
-  wizardFireball.addEventListener('click', function () {
-    wizardFireball.style.background = randomColor(fireballColor);
-    wizardFireballInput.value = randomColor(fireballColor);
-  });
-
-  //  реализация закрытия окна через щелчек мыши на buttonCloseSetup
-  buttonCloseSetup.addEventListener('click', function () {
     setupPopup.classList.add('hidden');
-  });
+  }
+});
 
-  //  реализация закрытия окна через нажатие кнопоки Enter при фокусе на buttonCloseSetup
-  buttonCloseSetup.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_KEYCODE) {
-      setupPopup.classList.add('hidden');
-    }
-  });
-};
+//  изменение цвета мантии по клику
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = randomColor(cloathColor);
+  wizardCoatInput.value = randomColor(cloathColor);
+});
+//  изменение цвета глаз по клику
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = randomColor(colorOfEyes);
+  wizardEyesInput.value = randomColor(colorOfEyes);
+});
+//  изменение цвета фаейрбола по клику
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = randomColor(fireballColor);
+  wizardFireballInput.value = randomColor(fireballColor);
+});
+
+//  реализация закрытия окна через щелчек мыши на buttonCloseSetup
+buttonCloseSetup.addEventListener('click', function () {
+  setupPopup.classList.add('hidden');
+});
+
+//  реализация закрытия окна через нажатие кнопоки Enter при фокусе на buttonCloseSetup
+buttonCloseSetup.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    setupPopup.classList.add('hidden');
+  }
+});
+
 //  реализация открытия окна через нажатие кнопоки Enter при фокусе на buttonOpenSetupIcon
 buttonOpenSetup.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
     setupPopup.classList.remove('hidden');
-    onButtonClick();
   }
 });
 
 //  реализация открытия окна через щелчек мыши на buttonOpenSetup
 buttonOpenSetup.addEventListener('click', function () {
   setupPopup.classList.remove('hidden');
-  onButtonClick();
 });
